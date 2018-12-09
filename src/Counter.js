198cbbb6ab4ff778a6982920a41aa0c8fe4d7e77
lubomir <lubomirkavetskiy@gmail.com>
2018-12-09 17:00:42 +0200

    Change comments

--- src/Counter.js
+++ src/Counter.js
@@ -4,10 +4,12 @@ export default class Counter extends Component {
   state = {
     // if to make like this: count: this.props.count || 0 => 
     // state will change by props, but it will not rerendr component
-    count: 0
+    count: 5,
+    // like this count : this.props.count => new this.props.count wont change this.state.count
+    // because it works only one time during constructor lifesycle, what is happened only one time
   }
 
-  // we use 'get' for don't call this function isControled() 
+  // using 'get' allows us call method (inside or outdoor class) like this `isControled`, without '()'
   get isControled() {
     return this.props.count !== undefined;
   }
@@ -15,7 +17,6 @@ export default class Counter extends Component {
   getState = () => this.isControled ? this.props : this.state;
 
   change = value => {
-    // 
     if (this.isControled) return this.props.onChange(this.getState().count + value);
     return this.setState({ count: this.getState().count + value });
   }
@@ -25,13 +26,14 @@ export default class Counter extends Component {
 
   render() {
     return (
-      <>
+      <p>
+        <span>{this.props.count !== undefined ? 'controlled component: ' : 'uncontrolled component: '}</span>
         <button onClick={this.decrement}>-</button>
         {/* we can't write like this: <span>{this.state.count}</span>,
         because component wont rerender after changing state */}
         <span>{this.getState().count}</span>
         <button onClick={this.increment}>+</button>
-      </>
+      </p>
     );
   }
 }
